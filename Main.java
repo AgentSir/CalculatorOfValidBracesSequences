6edd3d6be370a39df89bebe345d9880b764bca42
AgentSir <sputilov@ya.ru>
2015-06-12 09:34:00 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CountOfValidBracesSequencesCalculator calc = new CountOfValidBracesSequencesCalculator();
+
+        int numValidBracesSequences = calc.calculateCountOfValidBracesSequences();
+
+        System.out.println("Number of valid braces sequences = " + numValidBracesSequences);
+    }
+}
